18e5ba1ae10e9d81a8df10526e44f4fed9c3bc02
IanSanchezBaca <iansanchez066@gmail.com>
2025-05-17 18:54:21 -0700

    I fixed the link problem. IDK why i need to use the "Link" from next

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 // root page
-import Image from "next/image";
+// import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
+    
     <h>root page</h> <br></br>
-    {/* <link href="/temp">link to tempPage</link> */}
+    
+    <Link href="/temp">link to tempPage</Link>
+    
     </div>
-  
 );}
