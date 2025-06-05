103bf25d5f49d488987c6f890e9a72edf6a97942
IanSanchezBaca <iansanchez066@gmail.com>
2025-06-05 00:59:50 -0700

    added some comments and formatted

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,13 @@
-/* root page
- * import Image from "next/image";
-*/
-// import Link from "next/link"; // use this as using the regular link tag breaks
+/********************************************
+ * checkin/page.js 
+*********************************************/
 
+export default function Home() {
+  return (
+    <div className="myPadding">
 
+      <h1>Root Page</h1>
 
-export default function Home() {return (
-  <div className="myPadding">
-    
-    <h1>Root Page</h1> 
-    
-  </div>
-);}
+    </div>
+  );
+}
