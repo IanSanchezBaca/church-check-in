12baa8ce041dcba18947597ce338c1b8817a453b
IanSanchezBaca <iansanchez066@gmail.com>
2025-06-05 00:10:16 -0700

    figuring out the layout for this

--- src/app/checkIn/[kids]/page.js
+++ src/app/checkIn/[kids]/page.js
@@ -1,8 +1,13 @@
 /* This will probably hold all the stuff to do the stuff for the kids */
 
-export default function chick() {return(
-    <div>        
-        <h1>Main page for chick</h1>
+export default function chick() {
+    return (
+        <div>
+            <h1 className="myCenter">Kid Check In</h1>
+            <div className="smallPadding">
+                <h2>Parent</h2>
+            </div>
+        </div>
+    );
+}
 
-    </div>
-);}
