0bbd9a6ab59d6fb83aafc02cbcae8a790429c55e
IanSanchezBaca <iansanchez066@gmail.com>
2025-06-05 01:02:40 -0700

    added these pages

---
+++ src/app/checkIn/[kids]/[reg]/page.js
@@ -0,0 +1,19 @@
+/********************************************
+ * register/page.js 
+*********************************************/
+
+export default function reg() {
+    return (
+        <div>
+            <div className="myCenterX">
+                <h1>Kid Registration</h1>
+            </div>
+            <div className="smallPadding">
+                <h2>Parent</h2>
+                <label htmlFor="fname">First Name</label>
+                <input type="text"></input>
+            </div>
+        </div>
+    );
+}
+
---
+++ src/app/checkIn/[kids]/[signin]/page.js
@@ -0,0 +1,19 @@
+/********************************************
+ * signin/page.js 
+*********************************************/
+
+export default function signin() {
+    return (
+        <div>
+            <div className="myCenterX">
+                <h1>Kid Sign In</h1>
+            </div>
+            <div className="smallPadding">
+                <h2>Parent</h2>
+                <label htmlFor="fname">First Name</label>
+                <input type="text"></input>
+            </div>
+        </div>
+    );
+}
+
