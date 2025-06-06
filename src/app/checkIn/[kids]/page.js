e8c159081dddb9269915f168efbffbe0c47e6a6b
iansanchezbaca <iansanchez066@gmail.com>
2025-06-06 14:39:20 -0700

    made some changes so that the buttons look different when tehy are looked at on a phone

--- src/app/checkIn/[kids]/page.js
+++ src/app/checkIn/[kids]/page.js
@@ -12,12 +12,12 @@ export default function chick() {
                 <h1>Kids Check In</h1>
             </div>
             <div className="row">
-                <Link href="/checkIn/kids/signin" className="box" style={{ backgroundColor: "lightblue", color: "black" }}>
+                <Link href="/checkIn/kids/signin" className="box" style={{ backgroundColor: "#b4f2f1", color: "black" }}>
                     <div>
                         <h1>Sign In</h1>
                     </div>
                 </Link>
-                <Link href="/checkIn/kids/reg" className="box" style={{ backgroundColor: "lightblue", color: "black" }}>
+                <Link href="/checkIn/kids/reg" className="box" style={{ backgroundColor: "#b4f2f1", color: "black" }}>
                     <div>
                         <h1>Register</h1>
                     </div>
