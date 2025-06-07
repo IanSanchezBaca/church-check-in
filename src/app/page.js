5d39f4f556f12db6a2046fe14aad7f681d5c259a
iansanchezbaca <iansanchez066@gmail.com>
2025-06-06 19:51:11 -0700

    root page wont really be used for anything important

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,47 @@
  * root/page.js 
 *********************************************/
 
+
+// 'use client';
+
+// import { useEffect } from 'react';
+// import { db } from './lib/firebase';
+// import { collection, addDoc, getDocs } from 'firebase/firestore';
+
+// export default function TestFirestore() {
+//   useEffect(() => {
+//     const test = async () => {
+//       const ref = collection(db, 'testCollection');
+
+//       // Add a document
+//       await addDoc(ref, { name: 'Test User', timestamp: Date.now() });
+
+//       // Get all documents
+//       const snapshot = await getDocs(ref);
+//       snapshot.forEach(doc => {
+//         console.log(doc.id, doc.data());
+//       });
+//     };
+
+//     test();
+//   }, []);
+
+//   return <div>Check the console for Firestore test</div>;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default function Home() {
   return (
     <div className="myPadding">
