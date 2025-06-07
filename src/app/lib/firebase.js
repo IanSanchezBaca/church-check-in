8929cef48d4a48ca4e669516c22d10b409882f4e
iansanchezbaca <iansanchez066@gmail.com>
2025-06-06 18:01:22 -0700

    moved firebase into lib folder for orginization

---
+++ src/app/lib/firebase.js
@@ -0,0 +1,31 @@
+/********************************************
+ * lib/firebase.js 
+*********************************************/
+
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBz7AtIbpExBf_e7n-_BR52eajBeZsd6ZM",
+
+    authDomain: "chickendatabase-aee3a.firebaseapp.com",
+
+    projectId: "chickendatabase-aee3a",
+
+    storageBucket: "chickendatabase-aee3a.firebasestorage.app",
+
+    messagingSenderId: "434767814286",
+
+    appId: "1:434767814286:web:596782c3f18e94a53df3df",
+
+    measurementId: "G-SB3KW5LFRY"
+
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
