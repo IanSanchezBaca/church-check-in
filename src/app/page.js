4a6dbf75056b466b23417df1d50bfd9af504876c
IanSanchezBaca <iansanchez066@gmail.com>
2025-06-07 23:14:52 -0700

    actually using the root page for something

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 /********************************************
- * root/page.js 
+ * root/page.js
 *********************************************/
+'use client';
+import AuthButton from './lib/AuthButton';
 
 export default function Home() {
   return (
-    <div className="myPadding">
-
+    <div>
       <h1 style={{ backgroundColor: "aqua", textAlign: "center" }}>Root Page</h1>
-
+      <div style={{ textAlign: "right", marginRight: '10px' }}>
+        <AuthButton />
+      </div>
     </div >
   );
 }
