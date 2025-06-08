a283521cf4a7a76ac457bc7aa38598eb68839e05
IanSanchezBaca <iansanchez066@gmail.com>
2025-06-07 23:16:29 -0700

    login page works

---
+++ src/app/login/page.js
@@ -0,0 +1,95 @@
+/********************************************
+ * register/page.js 
+*********************************************/
+
+
+
+'use client'
+
+import { useState } from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '@/app/lib/firebase'
+import { useRouter } from 'next/navigation'
+
+export default function SignInPage() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const router = useRouter()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            await signInWithEmailAndPassword(auth, email, password)
+            router.push('/') // Redirect to admin dashboard
+        } catch (err) {
+            setError(err.message)
+        }
+    }
+
+    return (
+        <div>
+            <h1>Sign In</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email</label><br />
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password</label><br />
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+                <button type="submit">
+                    Login In
+                </button>
+            </form>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 'use client';
+// import React, { useState } from 'react'; /// This is used to create react function
+// import { createUserWithEmailAndPassword } from 'firebase/auth';
+// import { collection, addDoc } from 'firebase/firestore';
+// import { auth, db } from '@/app/lib/firebase';
+// import { useRouter } from 'next/navigation';
+
+
+// export default function MainLogin() {
+
+
+//     return React.createElement('div', {}, [
+//         React.createElement('h2', { key: 'headingkey' }, 'Login'),
+
+
+
+
+//     ]); // return
+
+// } // MainLogin
