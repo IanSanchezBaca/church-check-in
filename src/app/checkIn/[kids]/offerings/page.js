afd83aa7ef81150d233b7d9318cb6360e6d75d2a
iansanchezbaca <iansanchez066@gmail.com>
2025-07-21 20:15:55 -0700

    working on the offerings page

---
+++ src/app/checkIn/[kids]/offerings/page.js
@@ -0,0 +1,23 @@
+/********************************************
+ * kids/offerings/page.js
+ * This is a simple page where you 
+ * - can choose a date
+ * - update that date's total offering
+*********************************************/
+
+export default function OfferingsPage() {
+
+    return (
+        <div>
+            <h2>Offerings</h2>
+            <div>
+                <input
+                    placeholder="1.00"
+                />
+            </div>
+        </div>
+    ); // return
+
+
+
+}
