ee449047126e177c2f45318b7b5a36ee807577ef
iansanchezbaca <iansanchez066@gmail.com>
2025-07-27 09:27:08 -0700

    finally changed the color to green and added emoji

--- src/app/checkIn/[kids]/reports/page.js
+++ src/app/checkIn/[kids]/reports/page.js
@@ -247,7 +247,7 @@ export default function ReportsPage() {
                                     return (
                                         <div key={kidId} className="kdbcard">
                                             <p className="kdbkidid">
-                                                {kidName} {isNew && <span color='green'>New</span>}
+                                                {kidName} {isNew && <span style="color: green">🌟 New 🌟</span>}
                                             </p>
                                             <p className="kdbkidstatus">
                                                 Morning: {status.morning ? "✅" : "❌"}
